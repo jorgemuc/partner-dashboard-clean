732c794a1c3d73e4d6d4f909421f8cdf0cd21880
jorgemuc <36047598+jorgemuc@users.noreply.github.com>
2025-06-27 19:49:11 +0200

    Remove deprecated signing fields

--- __tests__/buildConfig.test.js
+++ __tests__/buildConfig.test.js
@@ -1,7 +1,7 @@
 const { test } = require('@jest/globals');
 const pkg = require('../package.json');
 
-test('windows build config disables signing', () => {
-  expect(pkg.build.win.sign).toBe(false);
-  expect(pkg.build.win).not.toHaveProperty('certificateSha1');
+test('windows build config has no signing fields', () => {
+  expect(pkg.build.win).not.toHaveProperty('sign');
+  expect(pkg.build.win).not.toHaveProperty('certificateSubjectName');
 });
