17ab6e688573e973d88723df6d5862e2f4ad3e99
jorgemuc <36047598+jorgemuc@users.noreply.github.com>
2025-06-27 12:04:24 +0200

    Limit CI to windows builds

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -4,33 +4,41 @@ const importGlob = require('esbuild-plugin-import-glob').default;
 const { version } = require('../package.json');
 const { mkdirSync, writeFileSync } = require('node:fs');
 
-mkdirSync('dist', { recursive: true });
+async function bundle() {
+  mkdirSync('dist', { recursive: true });
 
-esbuild.build({
-  entryPoints: ['src/preload.cjs'],
-  bundle: true,
-  minify: true,
-  platform: 'node',
-  format: 'cjs',
-  target: ['node16'],
-  outfile: 'dist/preload.js',
-  logLevel: 'info'
-}).catch(() => process.exit(1));
+  await Promise.all([
+    esbuild.build({
+      entryPoints: ['src/preload.cjs'],
+      bundle: true,
+      minify: true,
+      platform: 'node',
+      format: 'cjs',
+      target: ['node16'],
+      outfile: 'dist/preload.js',
+      logLevel: 'info'
+    }),
+    esbuild.build({
+      entryPoints: ['src/renderer/renderer.js'],
+      bundle: true,
+      minify: true,
+      treeShaking: true,
+      outfile: 'dist/renderer.bundle.js',
+      format: 'esm',
+      target: ['es2022'],
+      define: { 'process.env.NODE_ENV': '"production"' },
+      external: ['electron'],
+      plugins: [importGlob()],
+      sourcemap: true,
+      logLevel: 'info'
+    })
+  ]);
 
-esbuild.build({
-  entryPoints: ['src/renderer/renderer.js'],
-  bundle: true,
-  minify: true,
-  treeShaking: true,
-  outfile: 'dist/renderer.bundle.js',
-  format: 'esm',
-  target: ['es2022'],
-  define: { 'process.env.NODE_ENV': '"production"' },
-  external: ['electron'],
-  plugins: [importGlob()],
-  sourcemap: true,
-  logLevel: 'info'
-}).catch(() => process.exit(1));
+  writeFileSync('dist/version.json', JSON.stringify({ version }, null, 2) + '\n');
+  console.log('[bundle] wrote dist files');
+}
 
-writeFileSync('dist/version.json', JSON.stringify({ version }, null, 2) + '\n');
-console.log('[bundle] wrote dist files');
+bundle().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
